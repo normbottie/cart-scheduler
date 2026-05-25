d7d65f81851bcccc809f43f7c3c7c6981b8cd0a4
Norm Bottie <normbottie@MacBook-Air.local>
2026-05-25 18:02:46 -0400

    bust cache v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cart-scheduler-v6';
+const CACHE = 'cart-scheduler-v7';
 const ASSETS = ['./', './index.html', './app.js', './style.css', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
